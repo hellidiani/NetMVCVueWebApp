3809d70f9ea9cf718518dd4a21c5044575bf4fef
Moschetti <diana.moschetti@accenture.com>
2020-05-08 16:32:40 -0300

    Add project files.

---
+++ NetMVCVueWebApp/Scripts/Home/contactus.vue
@@ -0,0 +1,11 @@
+<template>
+    <div id="contactus">
+        <h1>Contact Us</h1>
+    </div>
+
+</template>
+<script>
+    export default {
+        name: "contact-component"
+    };
+</script>
---
+++ NetMVCVueWebApp/Scripts/Home/index.js
@@ -0,0 +1,13 @@
+﻿import Vue from 'vue';
+import HomeComponent from './home.vue';
+import ContactUsComponent from './contactus.vue';
+import AboutUsComponent from './aboutus.vue';
+
+new Vue({
+    el: "#app",
+    components: {
+        HomeComponent,
+        ContactUsComponent,
+        AboutUsComponent
+    }
+})
